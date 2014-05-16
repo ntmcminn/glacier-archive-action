8c13f70480aed14d99183520c1b43a871bd22b62
rgauss <rgauss@rgauss.com>
2014-05-16 17:08:46 -0400

    More Javadoc for ArchiveService

--- glacier-archive/glacier-archive-repo/src/main/java/org/alfresco/extension/archive/glacier/service/ArchiveService.java
+++ glacier-archive/glacier-archive-repo/src/main/java/org/alfresco/extension/archive/glacier/service/ArchiveService.java
@@ -9,11 +9,27 @@ import org.alfresco.service.cmr.repository.NodeRef;
 public interface ArchiveService
 {
     
+    /**
+     * Archives the given node ref into the given archive container, optionally 
+     * clearing out the local content stream object upon successful archive.
+     * 
+     * @param nodeRef the node ref to be archived
+     * @param archiveContainerIdentifier the container for the archive, i.e. Glacier vault
+     * @param clearLocalContentStream whether or not to clear out the local repo content stream
+     */
     public void archiveNode(
             NodeRef nodeRef, 
             String archiveContainerIdentifier, 
             boolean clearLocalContentStream);
     
+    /**
+     * Initiate the retrieval of the given node ref from the archive container.
+     * <p>
+     * Implementations must persist all data needed to retrieve the content as properties
+     * on the node.
+     * 
+     * @param nodeRef the node ref to be retrieved
+     */
     public void initiateRetrieval(NodeRef nodeRef);
 
 }
