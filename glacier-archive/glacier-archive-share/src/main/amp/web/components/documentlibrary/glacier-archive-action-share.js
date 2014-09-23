8953d6944e94147c2b6a3fee4879285a49f5db67
Nathan McMinn <nathan.mcminn@alfresco.com>
2014-09-23 13:26:40 -0700

    Unarchive action
    Added new code to support unarchive action

--- glacier-archive/glacier-archive-share/src/main/amp/web/components/documentlibrary/glacier-archive-action-share.js
+++ glacier-archive/glacier-archive-share/src/main/amp/web/components/documentlibrary/glacier-archive-action-share.js
@@ -103,4 +103,106 @@
 			
 	      }		
     });			
+	
+	YAHOO.Bubbling.fire("registerAction", {
+		actionName : "onGlacierUnArchiveAction",
+		fn: function onGlacierUnArchiveAction(record, owner) {
+			
+			this.widgets.waitDialog = Alfresco.util.PopupManager.displayMessage({
+				text : this.msg("message.glacier-unarchive.working"),
+				spanClass : "wait",
+				displayTime : 0
+			});
+			
+			// Get action params
+	         var params = this.getAction(record, owner).params,
+	            displayName = record.displayName,
+	            namedParams = ["function", "action", "success", "successMessage", "failure", "failureMessage"],
+	            repoActionParams = {};
+
+	         for (var name in params)
+	         {
+	            if (params.hasOwnProperty(name) && !Alfresco.util.arrayContains(namedParams, name))
+	            {
+	               repoActionParams[name] = params[name];
+	            }
+	         }
+
+	         // Deactivate action
+	         var ownerTitle = owner.title;
+	         owner.title = owner.title + "_deactivated";
+
+	         // Prepare genericAction config
+	         var config =
+	         {
+	            success:
+	            {
+	               event:
+	               {
+	                  name: "metadataRefresh",
+	                  obj: record
+	               }
+	            },
+	            failure:
+	            {
+	               message: this.msg(params.failureMessage, displayName),
+	               fn: function showAction()
+	               {
+	                  owner.title = ownerTitle;
+	               },
+	               scope: this
+	            },
+	            webscript:
+	            {
+	               method: Alfresco.util.Ajax.POST,
+	               stem: Alfresco.constants.PROXY_URI + "api/",
+	               name: "actionQueue"
+	            },
+	            config:
+	            {
+	               requestContentType: Alfresco.util.Ajax.JSON,
+	               dataObj:
+	               {
+	                  actionedUponNode: record.nodeRef,
+	                  actionDefinitionName: params.action,
+	                  parameterValues: repoActionParams
+	               }
+	            }
+	         };
+
+	         // Add configured success callbacks and messages if provided
+	         if (YAHOO.lang.isFunction(this[params.success]))
+	         {
+	            config.success.callback =
+	            {
+	               fn: this[params.success],
+	               obj: record,
+	               scope: this
+	            };
+	         }
+	         if (params.successMessage)
+	         {
+	            config.success.message = this.msg(params.successMessage, displayName);
+	         }
+
+	         // Acd configured failure callback and message if provided
+	         if (YAHOO.lang.isFunction(this[params.failure]))
+	         {
+	            config.failure.callback =
+	            {
+	               fn: this[params.failure],
+	               obj: record,
+	               scope: this
+	            };
+	         }
+	         if (params.failureMessage)
+	         {
+	            config.failure.message = this.msg(params.failureMessage, displayName);
+	         }
+
+	         // Execute the repo action
+	         this.modules.actions.genericAction(config);
+			
+	      }		
+    });
 })();
